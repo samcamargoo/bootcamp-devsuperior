83539a03f26a60e36c962e178a3a8482724820a1
Samuel Camargo <sam6camargo@gmail.com>
2021-09-08 16:10:43 -0300

    Basic static components Footer missing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import Datatable from 'components/Datatable';
 import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
 
 
 function App() {
   return (
     <>
-    <Navbar />
-    <h1 className="text-primary">Vendas</h1>
-    <Datatable />
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dasboard de vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm 6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+          </div>
+          </div>  
+
+        <Datatable />
+        
+      </div>
+      <Footer />
     </>
   );
 }
