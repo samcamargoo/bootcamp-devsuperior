8af9df684e2b22f2cff95ceec2685c8c6268a23d
Samuel Camargo <sam6camargo@gmail.com>
2021-09-09 13:43:59 -0300

    Pagination

--- backend/src/main/java/com/devsam/vendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsam/vendas/controllers/SaleController.java
@@ -1,26 +1,26 @@
 package com.devsam.vendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsam.vendas.dto.SaleDTO;
-import com.devsam.vendas.services.SaleService;
+import com.devsam.vendas.dto.SellerDTO;
+import com.devsam.vendas.services.SellerService;
 
 @RestController
-@RequestMapping(value = "/sales")
-public class SellerController {
+@RequestMapping(value = "/sellers")
+public class SaleController {
 	
 	@Autowired
-	private SaleService service;
+	private SellerService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
-		Page<SaleDTO> list = service.findAll(pageable);
+	public ResponseEntity<List<SellerDTO>> findAll() {
+		List<SellerDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
 		
 	}
